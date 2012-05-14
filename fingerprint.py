3e42991ed7e28f319d896a7d6ddc847e5744e112
Marc Brinkmann <git@marcbrinkmann.de>
2012-05-14 02:44:06 +0200

    Reworked large portion of tool.

--- fingerprint.py
+++ fingerprint.py
@@ -11,29 +11,39 @@ import time
 
 import msgpack
 import progressbar
+from remember.memoize import memoize, memoized_property
 
-PROGRESS_BAR_WIDGETS = ['Complete: ', progressbar.Percentage(), ' ',
+DATA_PROGRESS_BAR = ['Complete: ', progressbar.Percentage(), ' ',
                         progressbar.Bar(marker='#', left='[', right=']'),
                         ' ', progressbar.ETA(), ' ',
                         progressbar.FileTransferSpeed()]
 
-# queue based: process directory by queue'ing its children
-class FileMeta(object):
-    read_buf_size = 1024*1024*4
+class MetaBase(object):
+    """An object representing the metadata of an entity on the filesystem.
 
-    def __init__(self, filename):
-        self.filename = filename
+    :param path: The absolute path
+    """
 
-    def _get_stats(self):
-        if not hasattr(self, 's'):
-            self.update_stats()
+    def __init__(self, path):
+        self.path = path
+        self.children = []
 
-    def get_content_print(self):
+    @memoized_property
+    def s(self):
+        return os.lstat(self.path)
+
+    def __repr__(self):
+        return '%s(%r)' % (self.__class__.__name__, self.path)
+
+
+class FileMeta(MetaBase):
+    read_buf_size = 1024*1024*4  # 4M should be sufficient for speed and not
+                                 # too memory hungry
+
+    @memoized_property
+    def content_print(self):
         """Content prints rely only on the contents of the file - pretty much a
         'normal' application of the underlying hash function"""
-
-        self._get_stats()
-
         ftype = stat.S_IFMT(self.s.st_mode)
 
         if stat.S_IFLNK == ftype:
@@ -42,7 +52,7 @@ class FileMeta(object):
             h = sha1()
             remain = self.get_filesize()
 
-            with open(self.filename, 'rb') as src:
+            with open(self.path, 'rb') as src:
                 buf = True
                 while remain:
                     buf = src.read(min(self.read_buf_size, remain))
@@ -53,26 +63,127 @@ class FileMeta(object):
         else:
             raise Exception('Cannot handle filetype %s - sorry' % ftype)
 
-    def get_filesize(self):
-        self._get_stats()
+    @memoized_property
+    def filesize(self):
+        """Returns the filesize in bytes"""
         return self.s.st_size
 
-    def get_meta_print(self):
+    @memoized_property
+    def meta_print(self):
         """The meta print is a fingerprint based solely on the metadata of the
         file, not the contents"""
 
-        self._get_stats()
         stat_string = ' '.join(map(str, iter(self.s)))
-
         return sha1(stat_string).digest()
 
-    def update_stats(self):
-        self.s = os.lstat(self.filename)
-        return self.s
 
-    def __repr__(self):
-        return 'FM<%r>' % self.filename
+class DirMeta(MetaBase):
+    pass
 
+
+class Database(object):
+    def __init__(self, base):
+        self.base = base
+        self.meta_prints = {}
+        self.content_prints = {}
+
+    def dump(self, outfile):
+        db_dict = {
+            'meta_prints': self.meta_prints,
+            'content_prints': self.content_prints
+        }
+        msgpack.dump(db_dict, f)
+
+    def get_altered_files(self, fileset=None, progress=None):
+        fileset = fileset or self.files.keys()
+
+        altered = []
+
+        n_bytes = 0
+        for rel_name in fileset:
+            f = self.files[rel_name]
+            if rel_name in self.content_prints:
+                if f.content_print != self.content_print[rel_name]:
+                    altered.append(rel_name)
+
+            if progress:
+                n_bytes += f.filesize
+                progress(n_bytes)
+
+        return altered
+
+    def get_deleted_files(self):
+        return [rel_name for rel_name in self.meta_prints.iterkeys() if rel_name
+        not in self.files]
+
+    def get_new_and_updated_files(self, progress=None):
+        # determine which files have changed
+        # shortcut for empty db
+        if not self.meta_prints:
+            return self.files.keys(), []
+
+        new = []
+        updated = []
+
+        n_files = 0
+        for rel_name, file_meta in self.files.iteritems():
+            if progress:
+                progress(n_files)
+                n_files += 1
+
+            if not rel_name in self.meta_prints:
+                new.append(rel_name)
+                continue
+            if self.meta_prints[rel_name] != file_meta.meta_prints:
+                updated.append(rel_name)
+
+        return new, updated
+
+    def get_sizes_of(self, fileset):
+        return sum(self.files[rel_name].filesize for rel_name in fileset)
+
+    @classmethod
+    def load(cls, base, infile):
+        db_dict = msgpack.load(infile)
+
+        db = cls(base)
+        db.meta_prints = db_dict['meta_prints']
+        db.content_prints = db_dict['content_prints']
+
+        return db
+
+    def load_meta(self):
+        files = {}
+        dirs = {}
+
+        for root, ds, fs in os.walk(base):
+            rel_root = root[len(base)+1:]
+            root_meta = DirMeta(root)
+
+            for f in fs:
+                full_name = os.path.join(root, f)
+                rel_name = full_name[len(base)+1:]
+                f_meta = FileMeta(full_name)
+                files[rel_name] = f_meta
+                root_meta.children.append(f_meta)
+
+            dirs[rel_root] = root_meta
+
+        self.files = files
+        self.dirs = dirs
+
+def delay_filter(delay):
+    def _decorator(f):
+        last_update = 0
+
+        def _(*args, **kwargs):
+            cur = time.time()
+            if cur - last_update > delay:
+                last_update = cur
+                f(*args, **kwargs)
+        return _
+
+    return _decorator
 
 if '__main__' == __name__:
     import argparse
@@ -82,92 +193,42 @@ if '__main__' == __name__:
     parser.add_argument('--db', default='fingerprints.db')
     args = parser.parse_args()
 
-    # read database
+    base = os.path.abspath(args.directory)
+    print "Base directory: %s" % base
+
     if os.path.exists(args.db):
         print "Loading fingerprint database '%s'" % args.db
         with open(args.db, 'rb') as f:
-            old_db = msgpack.load(f)
-
-    # collect filenames
-    base = os.path.abspath(args.directory)
-    file_metas = {}
-    dirnames = []
-
-    for root, dirs, files in os.walk(base):
-        for f in files:
-            full_name = os.path.join(root, f)
-            rel_name = full_name[len(base)+1:]
-            file_metas[rel_name] = FileMeta(full_name)
-
-        for d in dirs:
-            dn = os.path.join(root, d)[len(base):]
-            dirnames.append(dn)
-
-    print "Collected %d files in %d directories" % (len(file_metas),
-                                                    len(dirnames))
-
-    print "Getting meta prints"
-    meta_prints = {name : fm.get_meta_print() for name, fm in
-                   file_metas.iteritems()}
-
-    # determine which files have changed
-    new_files = []
-    updated_meta_files = []
-    altered_files = []
-    if old_db:
-        print "Looking for changed files"
-        for fn, meta_print in meta_prints.iteritems():
-            if not fn in old_db['meta_prints']:
-                new_files.append(fn)
-            elif old_db['meta_prints'][fn] != meta_print:
-                updated_meta_files.append(fn)
+            db = Database.load(base, f)
     else:
-        raise Exception('FIXME')
+        print "New fingerprint database"
+        db = Database(base)
 
-    print "Found %d new files, checking %d old files for changes" % (
-        len(new_files), len(updated_meta_files)
+    # collect filenames on filesystem
+    db.load_meta()
+
+    print "Collected %d files in %d directories" % (len(db.files),
+                                                    len(db.dirs))
+
+    new, updated = db.get_new_and_updated_files()
+
+    # altered file checks with progress-bar
+    altered = []
+    if updated:  # could also force checking all files here with cmdline arg?
+        pbar = progressbar.ProgressBar(widgets=DATA_PROGRESS_BAR,
+                                       maxval=db.get_sizes_of(updated))
+        pbar.start()
+        altered = db.get_altered_files(updated, progress=pbar.progress)
+        pbar.finish()
+    deleted = db.get_deleted_files()
+
+    print "Found %d new files, %d updated, %d altered and %d deleted files" % (
+        len(new), len(updated), len(altered), len(deleted)
     )
 
-    candidates = [file_metas[name] for name in updated_meta_files]
+    sys.exit(0)
 
-    def collect_content_prints(fms, cb=None):
-        content_prints = {}
-
-        last_update = time.time()
-        n_bytes = 0
-        n_files = 0
-
-        for fm in fms:
-            cur_time = time.time()
-
-            if cb and cur_time - last_update > 1:
-                last_update = cur_time
-                cb(n_bytes, n_files)
-
-            content_prints[fm.filename] = fm.get_content_print()
-            n_bytes += fm.get_filesize()
-            n_files += 1
-
-        return content_prints
-
-    file_sizes_sum = sum(fm.get_filesize() for fm in file_metas.itervalues())
-    pbar = progressbar.ProgressBar(widgets=PROGRESS_BAR_WIDGETS,
-                                   maxval=file_sizes_sum)
-    pbar.start()
-    content_prints = collect_content_prints(candidates,
-                                            lambda n,_: pbar.udpate(n))
-    pbar.finish()
-
-    # see which files have changed and which haven't
-    for name, content_print in content_prints.iteritems():
-        if old_db['content_prints'][name] != content_print:
-            del updated_meta_files[name]
-            altered_files.append(name)
-
-    print "NEW", new_files
-    print "META", updated_meta_files
-    print "ALTERED", altered_files
-
+    # FIXME: properly handle transition from old meta to new meta
     print "Writing to database"
     db = {
         'meta_prints': meta_prints,
