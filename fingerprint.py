e306353ae567234651b5a7060f3ccfbae151bb1b
Marc Brinkmann <git@marcbrinkmann.de>
2012-05-17 16:32:45 +0200

    Added logging and docs to fingerprint.py.

--- fingerprint.py
+++ fingerprint.py
@@ -11,7 +11,10 @@ import time
 
 import msgpack
 import progressbar
+import logbook
 from remember.memoize import memoize, memoized_property
+
+log = logbook.Logger(__name__)
 
 DATA_PROGRESS_BAR = ['Complete: ', progressbar.Percentage(), ' ',
                         progressbar.Bar(marker='#', left='[', right=']'),
@@ -50,7 +53,7 @@ class FileMeta(MetaBase):
             return ''
         elif stat.S_IFREG == ftype:
             h = sha1()
-            remain = self.get_filesize()
+            remain = self.filesize
 
             with open(self.path, 'rb') as src:
                 buf = True
@@ -82,12 +85,25 @@ class DirMeta(MetaBase):
 
 
 class Database(object):
+    """Database object.
+
+    The database stores checksums of all files metadata and contents.
+    Subsequent runs can then compared the existing files to these collected
+    checksums and ensure only changes are backed up again.
+
+    A database keeps relative paths only. The folder being upload can therefore
+    be moved elsewhere.
+
+    :param base: The base path for the folder to be backed up. **Must** be an
+                 absolute path.
+    """
     def __init__(self, base):
         self.base = base
         self.meta_prints = {}
         self.content_prints = {}
 
     def dump(self, outfile):
+        """Write a serialized version of the database to filehandle."""
         db_dict = {
             'meta_prints': self.meta_prints,
             'content_prints': self.content_prints
@@ -95,6 +111,17 @@ class Database(object):
         msgpack.dump(db_dict, f)
 
     def get_altered_files(self, fileset=None, progress=None):
+        """Return a list of all files that have been altered (in comparison
+        with the loaded checksums).
+
+        An altered file is one that has had its contents changed.
+
+        :param fileset: If suppled, instead of checking all files, only check
+        the files whose relative names are in this list.
+        :param progress: Progress callback, called after every file is
+        processed with the total number of bytes checked so far.
+        :param return: List of relative names of files that have been altered.
+        """
         fileset = fileset or self.files.keys()
 
         altered = []
@@ -103,7 +130,7 @@ class Database(object):
         for rel_name in fileset:
             f = self.files[rel_name]
             if rel_name in self.content_prints:
-                if f.content_print != self.content_print[rel_name]:
+                if f.content_print != self.content_prints[rel_name]:
                     altered.append(rel_name)
 
             if progress:
@@ -113,11 +140,21 @@ class Database(object):
         return altered
 
     def get_deleted_files(self):
+        """Return a list of all files that are no longer present but have
+        records in the database.
+
+        :param return: List of relative names of files that have been deleted.
+        """
         return [rel_name for rel_name in self.meta_prints.iterkeys() if rel_name
         not in self.files]
 
     def get_new_and_updated_files(self, progress=None):
-        # determine which files have changed
+        """Return a list of all files whose metadata (stat) has changed.
+
+        :param progress: Progress callback, called after every file with the
+                         number of files processed.
+        :param return: List of relative names of files that have new metadata.
+        """
         # shortcut for empty db
         if not self.meta_prints:
             return self.files.keys(), []
@@ -134,16 +171,28 @@ class Database(object):
             if not rel_name in self.meta_prints:
                 new.append(rel_name)
                 continue
-            if self.meta_prints[rel_name] != file_meta.meta_prints:
+            if self.meta_prints[rel_name] != file_meta.meta_print:
                 updated.append(rel_name)
 
         return new, updated
 
     def get_sizes_of(self, fileset):
+        """Calculate the total size in bytes of all files in a set.
+
+        :param fileset: Relative names to files whose sizes are to be summed.
+        :return: Sum of filesizes in bytes.
+        """
         return sum(self.files[rel_name].filesize for rel_name in fileset)
 
     @classmethod
     def load(cls, base, infile):
+        """Unserialize a database from file.
+
+        :param base: Base path that all files are supposedly relative to.
+        :param infile: File object to read from.
+
+        :return: A :py:class:Database instance.
+        """
         db_dict = msgpack.load(infile)
 
         db = cls(base)
@@ -153,6 +202,11 @@ class Database(object):
         return db
 
     def load_meta(self):
+        """Loads all metadata (lstats) from the filesystem.
+
+        This should be called once for every database, after creating it and
+        before doing anything further with it.
+        """
         files = {}
         dirs = {}
 
@@ -172,6 +226,25 @@ class Database(object):
         self.files = files
         self.dirs = dirs
 
+    def update_meta(self):
+        """Replace the stored metadata with up-to-date info from the
+        filesystem."""
+
+        new_meta_prints = {}
+        new_content_prints = {}
+        for rel_name, file_meta in self.files.iteritems():
+            new_meta_prints[rel_name] = file_meta.meta_print
+
+            # if the metadata's the same, assume content hasn't changed either
+            if rel_name in self.meta_prints and\
+               self.meta_prints[rel_name] == file_meta.meta_print:
+                new_content_prints[rel_name] = self.content_prints[rel_name]
+            else:
+                new_content_prints[rel_name] = file_meta.content_print
+
+        self.meta_prints = new_meta_prints
+        self.content_prints = new_content_prints
+
 def delay_filter(delay):
     def _decorator(f):
         last_update = 0
@@ -189,26 +262,47 @@ if '__main__' == __name__:
     import argparse
 
     parser = argparse.ArgumentParser()
+    parser.set_defaults(loglevel=logbook.NOTICE)
     parser.add_argument('directory')
     parser.add_argument('--db', default='fingerprints.db')
+    parser.add_argument('-d', '--debug', action='store_true', default=False)
+
+    logargs = parser.add_mutually_exclusive_group()
+    logargs.add_argument('-v', '--verbose', const=logbook.INFO,
+                         action='store_const', dest='loglevel')
+    logargs.add_argument('-q', '--quiet', const=logbook.WARNING,
+                         action='store_const', dest='loglevel')
+
     args = parser.parse_args()
 
+    logbook.NullHandler().push_application()
+    logbook.StderrHandler(
+        level=logbook.DEBUG if args.debug else args.loglevel
+    ).push_application()
+
     base = os.path.abspath(args.directory)
-    print "Base directory: %s" % base
+    log.debug("Base directory: %s" % base)
 
     if os.path.exists(args.db):
-        print "Loading fingerprint database '%s'" % args.db
+        log.notice("Loading fingerprint database '%s'" % args.db)
         with open(args.db, 'rb') as f:
             db = Database.load(base, f)
     else:
-        print "New fingerprint database"
+        log.notice("New fingerprint database")
         db = Database(base)
+
+    if args.debug:
+        log.debug("META, CONTENT, RELNAME")
+        for rel_name, meta_print in db.meta_prints.iteritems():
+            log.debug('%s %s %s' % (hexlify(meta_print),\
+                  hexlify(db.content_prints[rel_name]),\
+                  rel_name))
 
     # collect filenames on filesystem
     db.load_meta()
 
-    print "Collected %d files in %d directories" % (len(db.files),
-                                                    len(db.dirs))
+    log.notice("Collected %d files in %d directories" % (len(db.files),
+                                                       len(db.dirs)))
 
     new, updated = db.get_new_and_updated_files()
 
@@ -218,24 +312,31 @@ if '__main__' == __name__:
         pbar = progressbar.ProgressBar(widgets=DATA_PROGRESS_BAR,
                                        maxval=db.get_sizes_of(updated))
         pbar.start()
-        altered = db.get_altered_files(updated, progress=pbar.progress)
+        altered = db.get_altered_files(updated, progress=pbar.update)
         pbar.finish()
     deleted = db.get_deleted_files()
 
-    print "Found %d new files, %d updated, %d altered and %d deleted files" % (
-        len(new), len(updated), len(altered), len(deleted)
-    )
+    log.notice("Found %d new files, %d updated, %d altered and %d deleted files"\
+             % (len(new), len(updated), len(altered), len(deleted)))
 
-    sys.exit(0)
+    if args.loglevel >= logbook.INFO:
+        for rel_name in new:
+            log.info("N %s" % rel_name)
+        for rel_name in updated:
+            log.info("U %s" % rel_name)
+        for rel_name in altered:
+            log.info("A %s" % rel_name)
+        for rel_name in deleted:
+            log.info("D %s" % rel_name)
 
-    # FIXME: properly handle transition from old meta to new meta
-    print "Writing to database"
-    db = {
-        'meta_prints': meta_prints,
-        'content_prints': content_prints
-    }
+    # FIXME: upload file data here
 
-    sys.exit(0)
+    # transition over
+    log.notice("Updating database")
+    db.update_meta()
+
+    # FIXME: upload database to s3?
+    log.debug("Writing to database")
 
     with open(args.db, 'wb') as f:
-        msgpack.dump(db, f)
+        db.dump(f)
